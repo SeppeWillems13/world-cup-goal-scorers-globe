a2a6e2f44a0bd0e6900aaf14d074c17121d8de79
SeppeWillems13 <willemss14@gmail.com>
2023-02-14 22:49:05 +0100

    add basic site

--- main.py
+++ main.py
@@ -1,45 +1,44 @@
-import csv
-import plotly.graph_objects as go
+import pandas as pd
+import streamlit as st
+import plotly.express as px
 
-# Read the CSV file
-with open("edited_cvs/worldcupgoals_1930_2022_BirthPlaceAndCountriesAndLatLong.csv", "r") as f:
-    csv_reader = csv.DictReader(f)
-    data = list(csv_reader)
+# Read in the CSV file as a DataFrame
+df = pd.read_csv('edited_cvs/worldcupgoals_1930_2022_BirthPlaceAndCountriesAndLatLong.csv', encoding='iso-8859-1')
 
-# Create a Scattergeo plot
-fig = go.Figure(
-    go.Scattergeo(
-        lat=[float(row["Latitude"]) for row in data],
-        lon=[float(row["Longitude"]) for row in data],
-        mode="markers",
-        marker=dict(
-            size=5,
-            color="blue",
-            opacity=0.7,
-        ),
-        showlegend=False,
-    )
-)
+# Sort the DataFrame by country in alphabetical order
+df = df.sort_values('Country', ascending=True)
 
-# Customize the plot
-fig.update_layout(
-    title=dict(
-        text="Birthplaces of World Cup Goal Scorers",
-        font=dict(size=24),
-    ),
-    geo=dict(
-        projection_type="natural earth",
-        landcolor="rgb(217, 217, 217)",
-        oceancolor="rgb(204, 255, 255)",
-        showocean=True,
-        showland=True,
-        showcountries=True,
-        countrywidth=0.5,
-        showcoastlines=True,
-        coastlinecolor="white",
-        coastlinewidth=0.5,
-    ),
-)
+# Add a name search input
+name_search = st.sidebar.text_input("Search for a player by name:")
 
-# Show the plot
-fig.show()
+# Filter the DataFrame based on name search input
+if name_search:
+    df = df[df['Player'].str.contains(name_search, case=False)]
+
+# Add a slider to filter by goals scored
+goals = st.sidebar.slider("Filter by number of goals scored:", min_value=0, max_value=20, value=0)
+
+# Filter the DataFrame based on goals scored
+df = df[df['Goals'] >= goals]
+
+# Add a checkbox to show goalscorers born outside of the country they played for
+show_foreign = st.sidebar.checkbox("Show goalscorers born outside of the country they played for")
+
+# Filter the DataFrame based on the checkbox selection
+if not show_foreign:
+    df = df[df['Country'] == df['CountryOfBirth']]
+
+# Create a Plotly chart
+fig = px.scatter_geo(df, lat="Latitude", lon="Longitude", color="Country",
+                     hover_name="Player", size="Goals",
+                     projection="natural earth", scope='world',
+                     hover_data={'CountryOfBirth': False, 'Country': True, 'Goals': True})
+
+# Add country lines to the chart
+fig.update_geos(showcountries=True, countrycolor="Black", showsubunits=True, subunitcolor="Blue")
+
+#title
+st.title("World Cup Goals by Birth Country")
+
+# Display the chart in Streamlit app
+st.plotly_chart(fig)
